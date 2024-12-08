3b66b0e60c4d4a6c262efc9c5b325e4655d2ef19
Ailrk <jimmy123good@gmail.com>
2024-12-08 02:07:00 -0500

    Add submodules for nixosModule

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,64 +1,85 @@
 self: { config, lib, pkgs, ...}:
 let
-  cfg = config.services.filehub;
+  cfgs = config.services.filehub;
 in
 {
-  options.services.filehub = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Enable filehub
-      '';
-    };
+  options.services.filehub =
+  let submodule = { config, ... }:
+    {
+      options = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = ''
+            Enable filehub
+          '';
+        };
 
-    root = lib.mkOption {
-      type = lib.types.path;
-      default = "";
-      description = ''
-        Root folde to serve.
-      '';
-    };
+        root = lib.mkOption {
+          type = lib.types.path;
+          default = "";
+          description = ''
+            Root folde to serve.
+          '';
+        };
 
-    port = lib.mkOption {
-      type = lib.types.int;
-      default = 5000;
-      description = ''
-        Port to serve
-      '';
-    };
+        port = lib.mkOption {
+          type = lib.types.int;
+          default = 5000;
+          description = ''
+            Port to serve
+          '';
+        };
 
-    environment = lib.mkOption {
-      type = lib.types.attrsOf (lib.types.either lib.types.str lib.types.path);
-      default = {};
-      description = ''
-        Environment variables for filehub.
-      '';
-    };
+        environment = lib.mkOption {
+          type = lib.types.attrsOf (lib.types.either lib.types.str lib.types.path);
+          default = {};
+          description = ''
+            Environment variables for filehub.
+          '';
+        };
 
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = self.packages.${pkgs.system}.default;
-      description = ''
-        The filehub package to use.
-      '';
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    systemd.services.filehub = {
-      wantedBy = [  "multi-user.target" ];
-      after = [ "network.target" ];
-      description = "Start filehub.";
-      environment = cfg.environment;
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig = {
-        Type = "simple";
-        EnvironmentFile = cfg.secretFile;
-        ExecStart = [
-          "${cfg.package}/bin/filehub --port ${cfg.port} --root ${cfg.root}"
-        ];
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.packages.${pkgs.system}.default;
+          description = ''
+            The filehub package to use.
+          '';
+        };
       };
     };
+  in
+    lib.mkOption {
+      description = "Configure filehub instances.";
+      default = {};
+      type = lib.types.attrsOf (lib.types.submodule submodule);
+    };
+
+
+  config = lib.mkIf (lib.any (cfg: cfg.enable) (lib.attrValues cfgs)) {
+    systemd.services =
+      let
+        mkService = name: cfg:
+        let
+          serviceName = "filehub-" + name;
+          port = builtins.toString cfg.port;
+        in
+          lib.nameValuePair
+            serviceName
+            {
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              description = "Start ${serviceName}";
+              unitConfig.DefaultDependencies = "no";
+              path = [ pkgs.getent ];
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = ''
+                  "${cfg.package}/bin/filehub --port ${port} --root ${cfg.root}"
+                '';
+              };
+            };
+      in
+      lib.mapAttrs' mkService cfgs ;
   };
 }
