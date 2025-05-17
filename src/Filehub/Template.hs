db663158c0b471976050cb103bbb4cbc0756ad96
Ailrk <jimmy123good@gmail.com>
2025-05-16 17:07:01 -0700

    Change modified date format

--- src/Filehub/Template.hs
+++ src/Filehub/Template.hs
@@ -645,7 +645,7 @@ table target root files selected order = do
                , title_ (Text.pack displayTime)
                ]
       where
-        displayTime = maybe mempty (formatTime defaultTimeLocale "%F %R") file.mtime
+        displayTime = maybe mempty (formatTime defaultTimeLocale "%d %b %y") file.mtime
 
 
     fileNameElement :: File -> Html ()
