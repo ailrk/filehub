abae602e91347ca46751418f67192578ed0777c5
Ailrk <jimmy123good@gmail.com>
2025-08-05 15:47:03 -0700

    Update cabal

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, aeson-pretty, amazonka, amazonka-s3, async
 , base, bytestring, case-insensitive, conduit, containers, cookie
-, cryptohash-sha256, directory, effectful, filepath, fuzzy
-, generic-lens, hashable, hashtables, hspec, hspec-wai
+, cryptohash-sha256, directory, effectful, file-embed, filepath
+, fuzzy, generic-lens, hashable, hashtables, hspec, hspec-wai
 , http-api-data, http-types, lib, log-base, log-effectful, lucid
 , microlens, microlens-platform, mime-types, network-uri
 , optparse-applicative, QuickCheck, servant, servant-conduit
@@ -20,13 +20,13 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson aeson-pretty amazonka amazonka-s3 async base bytestring
     case-insensitive conduit containers cookie cryptohash-sha256
-    directory effectful filepath fuzzy generic-lens hashable hashtables
-    http-api-data http-types log-base log-effectful lucid microlens
-    microlens-platform mime-types network-uri optparse-applicative
-    servant servant-conduit servant-lucid servant-multipart
-    servant-server string-interpolate suspend text time timers
-    transformers unix unliftio unordered-containers uri-encode uuid
-    vault wai wai-app-static wai-extra warp zip-archive
+    directory effectful file-embed filepath fuzzy generic-lens hashable
+    hashtables http-api-data http-types log-base log-effectful lucid
+    microlens microlens-platform mime-types network-uri
+    optparse-applicative servant servant-conduit servant-lucid
+    servant-multipart servant-server string-interpolate suspend text
+    time timers transformers unix unliftio unordered-containers
+    uri-encode uuid vault wai wai-app-static wai-extra warp zip-archive
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
