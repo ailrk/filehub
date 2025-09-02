a3b58003d30c6a53106b30216cdecf1deee53eee
Ailrk <jimmy123good@gmail.com>
2025-09-01 19:33:43 -0700

    Update flake

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
 { mkDerivation, aeson, aeson-pretty, amazonka, amazonka-s3, async
-, base, bcrypt, bytestring, case-insensitive, conduit, containers
-, cookie, criterion, cryptohash-sha256, data-default, directory
-, effectful, file-embed, filepath, fuzzy, generic-lens, hashable
-, hashtables, hspec, hspec-wai, http-api-data, http-types
-, JuicyPixels, JuicyPixels-stbir, jwt, lib, log-base, log-effectful
-, lucid, microlens, microlens-platform, mime-types, network-uri
+, base, base64-bytestring, bcrypt, bytestring, case-insensitive
+, conduit, containers, cookie, criterion, cryptohash-sha256
+, data-default, directory, effectful, file-embed, filepath, fuzzy
+, generic-lens, hashable, hashtables, hspec, hspec-wai
+, http-api-data, http-client, http-types, JuicyPixels
+, JuicyPixels-stbir, jwt, lib, log-base, log-effectful, lucid
+, microlens, microlens-platform, mime-types, network-uri
 , optparse-applicative, process, QuickCheck, random, req, servant
-, servant-conduit, servant-lucid, servant-multipart, servant-server
-, split, string-interpolate, suspend, temporary, text, time, timers
-, tomland, transformers, unliftio, unordered-containers, uri-encode
-, uuid, vault, wai, wai-app-static, wai-extra, warp, zip
+, servant-client, servant-conduit, servant-lucid, servant-multipart
+, servant-server, split, string-interpolate, suspend, temporary
+, text, time, timers, tomland, transformers, unliftio
+, unordered-containers, uri-encode, uuid, vault, wai
+, wai-app-static, wai-extra, warp, zip
 }:
 mkDerivation {
   pname = "filehub";
@@ -19,13 +21,14 @@ mkDerivation {
   isExecutable = true;
   enableSeparateDataOutput = true;
   libraryHaskellDepends = [
-    aeson aeson-pretty amazonka amazonka-s3 async base bcrypt
-    bytestring case-insensitive conduit containers cookie
-    cryptohash-sha256 data-default directory effectful file-embed
-    filepath fuzzy generic-lens hashable hashtables http-api-data
-    http-types JuicyPixels JuicyPixels-stbir jwt log-base log-effectful
-    lucid microlens microlens-platform mime-types network-uri
-    optparse-applicative random servant servant-conduit servant-lucid
+    aeson aeson-pretty amazonka amazonka-s3 async base
+    base64-bytestring bcrypt bytestring case-insensitive conduit
+    containers cookie cryptohash-sha256 data-default directory
+    effectful file-embed filepath fuzzy generic-lens hashable
+    hashtables http-api-data http-client http-types JuicyPixels
+    JuicyPixels-stbir jwt log-base log-effectful lucid microlens
+    microlens-platform mime-types network-uri optparse-applicative
+    random servant servant-client servant-conduit servant-lucid
     servant-multipart servant-server split string-interpolate suspend
     temporary text time timers tomland transformers unliftio
     unordered-containers uri-encode uuid vault wai wai-app-static
